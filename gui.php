6e9035a9c89e51d5bc6c7569065ab6f573f05f56
donggyun00 <dongkunkim00@gmail.com>
2018-11-26 11:37:00 +0900

    no message

--- gui.php
+++ gui.php
@@ -1,17 +1,11 @@
 <?php
-<<<<<<< HEAD
 	echo "hyop30@gmail.com";
-=======
-    echo "hyop30@gmail.com";
 	echo "<br />";
->>>>>>> 1176f862f26c9ce0e325ce6a3817da7b501f15d6
 	echo "dongkunkim00@gmail.com";
 	echo "<br />";
-	echo "andyahn1991@gmail.com"; 
-<<<<<<< HEAD
-	echo "naim199352@gmail.com";
-=======
+	echo "andyahn1991@gmail.com";
 	echo "<br />";
-    echo "naim199352@gmail.com";
->>>>>>> 1176f862f26c9ce0e325ce6a3817da7b501f15d6
+	echo "naim199352@gmail.com";
+	echo "<br />";
+	echo "naim199352@gmail.com";
 ?>
